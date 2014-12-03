8064666553e6acb0e8d639d7ff5e7d1a66611f4a
mudslinger <hisato.tanaka@gmail.com>
2014-12-03 12:06:06 +0900

    'tightvnc'

--- modules/vnc/manifests/init.pp
+++ modules/vnc/manifests/init.pp
@@ -1,8 +1,8 @@
 class vnc inherits setup{
-  $vncpath = "$setupdir\VNC-Server-5.2.1-Windows-en-64bit.msi"
+  $vncpath = "$setupdir\tightvnc-2.7.10-setup-64bit.msi"
   file{ $vncpath:
     ensure => present,
-    source => "puppet:///files/VNC-Server-5.2.1-Windows-en-64bit.msi",
+    source => "puppet:///files/tightvnc-2.7.10-setup-64bit.msi",
     source_permissions => ignore,
     require => File[$setupdir]
   }
@@ -10,7 +10,14 @@ class vnc inherits setup{
     ensure => installed,
     provider => "windows",
     source => $vncpath,
-    install_options => ["/qn"],
+    install_options => [
+      "/quiet",
+      "/norestart",
+      "ADDLOCAL=Server",
+      'SET_IPACCESSCONTROL=192.168.0.0-192.168.255.255:0',
+      "SET_USECONTROLAUTHENTICATION=1",
+      "SET_CONTROLPASSWORD=P@ssw0rd"
+    ],
     require => File[$vncpath]
   }
 }
