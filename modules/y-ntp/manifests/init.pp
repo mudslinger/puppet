84d523762f2e99f51f0d3a690d8fccbcf13df7dd
mudslinger <hisato.tanaka@gmail.com>
2014-12-03 09:58:48 +0900

    'ntpfix2'

--- modules/y-ntp/manifests/init.pp
+++ modules/y-ntp/manifests/init.pp
@@ -1,7 +1,11 @@
 ########################
 # Win-NTP
 ########################
+
+include 'winntp'
 class y-ntp inherits winntp{
-  special_poll_interval => 1800,
-  ntp_server => 'artemis.yamaokaya.local'
+  class {'winntp:'
+    special_poll_interval => 1800,
+    ntp_server => 'artemis.yamaokaya.local'
+  }
 }
