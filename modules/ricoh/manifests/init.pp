f0e63e5bafd6e53c7b4e12cb005f9e3aade1ffa1
mudslinger <hisato.tanaka@gmail.com>
2014-12-04 16:09:31 +0900

    'grv'

--- modules/ricoh/manifests/init.pp
+++ modules/ricoh/manifests/init.pp
@@ -3,14 +3,14 @@
 ########################
 include setup
 class ricoh($printer_ip = "192.168.128.8") inherits setup{
-  $monopath = "$parent::setupdir\176757"
-  $colorpath = "$parent::setupdir\176758"
+  $monopath = "$setupdir\176757"
+  $colorpath = "$setupdir\176758"
   file{ $monopath:
     ensure => present,
     source => "puppet:///files/176757",
     source_permissions => ignore,
     recurse => true,
-    require => File[$parent::setupdir]
+    require => File[$setupdir]
   }
 
   file{ $colorpath:
@@ -18,7 +18,7 @@ class ricoh($printer_ip = "192.168.128.8") inherits setup{
     source => "puppet:///files/176758",
     source_permissions => ignore,
     recurse => true,
-    require => File[$parent::setupdir]
+    require => File[$setupdir]
   }
 
   exec{ "install c305 driver":
