2a760c105fcc570ca56d923a9f8883c9d5bff701
mudslinger <hisato.tanaka@gmail.com>
2014-12-04 16:02:33 +0900

    'hiera'

--- modules/ricoh/manifests/init.pp
+++ modules/ricoh/manifests/init.pp
@@ -1,6 +1,7 @@
 ########################
 #printer driver Install
 ########################
+include setup
 class ricoh($printer_ip = "192.168.128.8") inherits setup{
 
   file{ $monopath:
@@ -8,7 +9,7 @@ class ricoh($printer_ip = "192.168.128.8") inherits setup{
     source => "puppet:///files/176757",
     source_permissions => ignore,
     recurse => true,
-    require => File[$setupdir]
+    require => File[$parent::setupdir:]
   }
 
   file{ $colorpath:
@@ -16,7 +17,7 @@ class ricoh($printer_ip = "192.168.128.8") inherits setup{
     source => "puppet:///files/176758",
     source_permissions => ignore,
     recurse => true,
-    require => File[$setupdir]
+    require => File[$parent::setupdir:]
   }
 
   exec{ "install c305 driver":
