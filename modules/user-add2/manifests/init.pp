0da4f00b3351b486ddf379a34ad900325f27b50e
mudslinger <hisato.tanaka@gmail.com>
2014-12-08 16:02:03 +0900

    '321'

--- modules/user-add2/manifests/init.pp
+++ modules/user-add2/manifests/init.pp
@@ -1,13 +1,13 @@
 class user-add2(
 	$users = []
 ){
-  each($users) |$user| {
+  each($users) |$value| {
 		exec{ "user-add2":
 			#command => template('user-add2/user-add.ps1.erb'),
-			command => "net user $user.staff_id '$user.staff_password' /add /expires:never /passwordchg:no",
+			command => "net user $value.staff_id '$value.staff_password' /add /expires:never /passwordchg:no",
 			provider => powershell,
 			onlyif => [
-				"if((net user | Out-String).IndexOf('$user.staff_id') -lt 0){exit 0}elese{exit 1}"
+				"if((net user | Out-String).IndexOf('$value.staff_id') -lt 0){exit 0}elese{exit 1}"
 			],
 			logoutput => true
 		}
