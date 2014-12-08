c544ea13ca36084317069f93bad7308649072c8a
mudslinger <hisato.tanaka@gmail.com>
2014-12-08 17:03:09 +0900

    '123123'

--- modules/user-add2/manifests/init.pp
+++ modules/user-add2/manifests/init.pp
@@ -1,15 +1,13 @@
 class user-add2(
 	$users = []
 ){
-  each($users) |$value| {
-		exec{ "user-add2":
-			#command => template('user-add2/user-add.ps1.erb'),
-			command => "net user $value.staff_id '$value.staff_password' /add /expires:never /passwordchg:no",
-			provider => powershell,
-			onlyif => [
-				"if((net user | Out-String).IndexOf('$value.staff_id') -lt 0){exit 0}elese{exit 1}"
-			],
-			logoutput => true
-		}
-  }
+	exec{ "user-add2":
+		#command => template('user-add2/user-add.ps1.erb'),
+		command => "net user $value.staff_id '$value.staff_password' /add /expires:never /passwordchg:no",
+		provider => powershell,
+		onlyif => [
+			"if((net user | Out-String).IndexOf('$value.staff_id') -lt 0){exit 0}elese{exit 1}"
+		],
+		logoutput => true
+	}
 }
