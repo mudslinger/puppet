f75a19d279e3f598b189d6dea66eec6bb305f9ac
mudslinger <hisato.tanaka@gmail.com>
2014-12-09 13:51:51 +0900

    '1231'

--- modules/user-add2/manifests/init.pp
+++ modules/user-add2/manifests/init.pp
@@ -5,9 +5,6 @@ class user-add2(
 		command => template('user-add2/user-add.ps1.erb'),
 		#command => "net user $user_id '$user_passwd' /add /expires:never /passwordchg:no",
 		provider => powershell,
-#		onlyif => [
-#			"if((net user | Out-String).IndexOf('$user_id') -lt 0){exit 0}elese{exit 1}"
-#		],
 		logoutput => true
 	}
 }
