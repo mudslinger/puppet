8946e099477710e547633a8048c10b50f51a8545
mudslinger <hisato.tanaka@gmail.com>
2014-12-10 12:07:02 +0900

    'a'

--- modules/misc-registry/manifests/init.pp
+++ modules/misc-registry/manifests/init.pp
@@ -5,33 +5,33 @@ class misc-registry{
     type => dword,
     data => 1
   }
-  registry::value { "www3.fujipan.co.jp"
+  registry::value { "www3.fujipan.co.jp":
     key => "HKLM\Software\Policies\Microsoft\Internet Explorer\BrowserEmulation\PolicyList",
     type => string,
     data => "www3.fujipan.co.jp"
   }
   #IEホームページ
-  registry::value { "Start Page"
+  registry::value { "Start Page":
     key => "HKLM\Software\Microsoft\Internet Explorer\Main",
     type => string,
     data => "http://ec9.winboard.jp"
   }
-  registry::value { "Secondary Start Pages"
+  registry::value { "Secondary Start Pages":
     key => "HKLM\Software\Microsoft\Internet Explorer\Main",
     type => array,
     data => ["https://yamaokaya1.sharepoint.com","https://www.google.co.jp"]
   }
-  registry::value { "Search Page"
+  registry::value { "Search Page":
     key => "HKLM\Software\Microsoft\Internet Explorer\Main",
     type => string,
     data => "https://www.google.co.jp"
   }
-  registry::value { "Search Bar"
+  registry::value { "Search Bar":
     key => "HKLM\Software\Microsoft\Internet Explorer\Main",
     type => string,
     data => "https://www.google.co.jp/ie"
   }
-  registry::value { "SearchURL"
+  registry::value { "SearchURL":
     key => "HKLM\Software\Microsoft\Internet Explorer\SearchURL",
     type => string,
     data => "https://www.google.co.jp/search?q=%s"
