704900902ce6460fd90ae3ffe01acde268830584
mudslinger <hisato.tanaka@gmail.com>
2014-12-10 20:05:16 +0900

    'a'

--- modules/netlogon/manifests/init.pp
+++ modules/netlogon/manifests/init.pp
@@ -15,7 +15,7 @@ class netlogon($netlogondir = "c:\\setup\netlogon") inherits setup{
   exec { "share-netlogon":
   	command => "New-SmbShare -Name NETLOGON -Path '$netlogondir'",
   	provider => powershell,
-  	onlyif => "if(Get-SmbShare -Name NETLOGON){exit 1}else{exit 0}"
+  	onlyif => "if(Get-SmbShare -Name NETLOGON){exit 1}else{exit 0}",
   	logoutput => true,
   	require => [File["$netlogondir\\logon.ps1"],File["$netlogondir\\logon.bat"]]
   }
