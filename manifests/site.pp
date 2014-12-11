bddfbb4a164fcfacfcb3eb77055fc80058f9e318
mudslinger <hisato.tanaka@gmail.com>
2014-12-11 18:09:33 +0900

    '123'

--- manifests/site.pp
+++ manifests/site.pp
@@ -41,7 +41,7 @@ node /^s9999\-pc.*/ inherits default{
     users => [
       {id => 'Adiministrator',passwd => 'P@ssw0rd',group => 'administrators'},
       {id => 't.kaneko',passwd => 'k30n718',group => 'administrators'},
-      {id => 'staff-1255', passwd => ''}
+      {id => 'staff-1255', passwd => '',group => 'HomeUsers'}
     ]
   }
 }
