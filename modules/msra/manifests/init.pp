5eab0354b6503eabafaf534dee5d9209408e6cad
mudslinger <hisato.tanaka@gmail.com>
2014-12-15 15:13:01 +0900

    '123123'

--- modules/msra/manifests/init.pp
+++ modules/msra/manifests/init.pp
@@ -1,7 +1,7 @@
 class msra{
   $pos = "c:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs"
-	file{"$pos\\リモートヘルプ".bat":
-	  ensure => "present",
-	  content => template('msra/help.bat.erb')
+  file{"$pos\\リモートヘルプ.bat":
+		ensure => "present",
+		content => template('msra/help.bat.erb')
   }
 }
