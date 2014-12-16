cd3f376144655a38c5f0151cb6de83a4f2c0ed3a
mudslinger <hisato.tanaka@gmail.com>
2014-12-16 19:42:26 +0900

    'pinn'

--- modules/msra/manifests/init.pp
+++ modules/msra/manifests/init.pp
@@ -1,9 +1,9 @@
 class msra{
-  $pos = "c:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs"
-  file{"$pos\\remotehelp.bat":
+	$pos = "c:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs"
+	file{"$pos\\remotehelp.bat":
 		ensure => "present",
 		content => template('msra/help.bat.erb')
-  }
+	}
 	exec{ "msra_firewall":
 		provider => powershell,
 		command => template('msra/add_firewall.ps1'),
