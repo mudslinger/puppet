4f28a6abd74f890ae46c21826d20f874e9759bcb
mudslinger <hisato.tanaka@gmail.com>
2014-12-16 14:17:55 +0900

    'msra_firewall'

--- modules/puppetconf/manifests/init.pp
+++ modules/puppetconf/manifests/init.pp
@@ -2,6 +2,6 @@ class puppetconf {
 	$puppetconfpath = "C:\\ProgramData\\PuppetLabs\\puppet\\etc\\puppet.conf"
 	file{ $puppetconfpath:
 	  ensure => present,
-	  source => template('puppetconf/puppet.conf')
+	  content => template('puppetconf/puppet.conf')
 	}
 }
