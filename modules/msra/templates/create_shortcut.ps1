61ceb40b9ebb65522e2165933a8e5a203530cecb
mudslinger <hisato.tanaka@gmail.com>
2014-12-16 19:25:33 +0900

    'pinn'

--- modules/msra/templates/create_shortcut.ps1
+++ modules/msra/templates/create_shortcut.ps1
@@ -2,7 +2,8 @@ $shell = New-Object -comObject WScript.Shell
 
 $path = [Environment]::GetFolderPath('CommonApplicationData') + '\Microsoft\Windows\Start Menu\Programs'
 $cmd = [Environment]::GetFolderPath('System') + '\cmd.exe' 
+if(-not(Test-Path "$path/リモートヘルプ要請.lnk")){
 $shortcut = $shell.CreateShortcut("$path/リモートヘルプ要請.lnk")
-$shortcut.TargetPath = "$cmd /C $path\リモートヘルプ.bat"
-
+$shortcut.TargetPath = "$cmd /C $path\remotehelp.bat"
 $shortcut.save()
+}
