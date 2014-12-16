7350d332eec2e02c29efd2e88e11f68cc5bb4a26
mudslinger <hisato.tanaka@gmail.com>
2014-12-16 20:16:50 +0900

    'remote'

--- modules/msra/templates/create_shortcut.ps1
+++ modules/msra/templates/create_shortcut.ps1
@@ -1,9 +1,11 @@
-$shell = New-Object -comObject WScript.Shell
+﻿$shell = New-Object -ComObject WScript.Shell
 
-$path = [Environment]::GetFolderPath('CommonApplicationData') + '\Microsoft\Windows\Start Menu\Programs'
-$cmd = [Environment]::GetFolderPath('System') + '\cmd.exe' 
-if(-not(Test-Path "$path\remotehelp.lnk")){
-$shortcut = $shell.CreateShortcut("$path\remotehelp.lnk")
-$shortcut.TargetPath = "$cmd /C $path\remotehelp.bat"
-$shortcut.save()
+$path = [Environment]::GetFolderPath("CommonApplicationData") + "\Microsoft\Windows\Start Menu\Programs"
+$cmd = [Environment]::GetFolderPath("System") + "\cmd.exe"
+$dest = "$path\本部へリモートメンテ要請.lnk"
+if(-not(Test-Path $dest)){
+    $shortcut = $shell.CreateShortcut($dest)
+    $shortcut.TargetPath = $cmd
+    $shortcut.Arguments = "/C '$path\remotehelp.bat'"
+    $shortcut.save()
 }
