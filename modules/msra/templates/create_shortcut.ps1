6879a3feedf8342f7771f9dad1d2be56e6e29883
mudslinger <hisato.tanaka@gmail.com>
2014-12-17 10:52:30 +0900

    '321'

--- modules/msra/templates/create_shortcut.ps1
+++ modules/msra/templates/create_shortcut.ps1
@@ -1,11 +1,11 @@
 $shell = New-Object -ComObject WScript.Shell
 $path = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs"
 $cmd = [Environment]::GetFolderPath("System") + "\cmd.exe"
-$dest = "$path\honbu-remote.lnk"
+$dest = "$path\本部remote要請.lnk"
 $shortcut = $shell.CreateShortcut($dest)
 $shortcut.TargetPath = $cmd
 $shortcut.Arguments = "/C C:\setup\remotehelp.bat"
 $shortcut.WorkingDirectory = "c:\setup"
-$shortcut.IconLocation = [Environment]::GetFolderPath("System") + "msra.exe,0"
+$shortcut.IconLocation = "msra.exe,0"
 $shortcut.WindowStyle = 7
 $shortcut.save()
