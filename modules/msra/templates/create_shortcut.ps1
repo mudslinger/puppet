7cdb7fabd2d5472b3a638f435dc05bdfd78020cc
mudslinger <hisato.tanaka@gmail.com>
2014-12-17 09:28:43 +0900

    '123'

--- modules/msra/templates/create_shortcut.ps1
+++ modules/msra/templates/create_shortcut.ps1
@@ -1,10 +1,8 @@
 $shell = New-Object -ComObject WScript.Shell
 
-$path = [Environment]::GetFolderPath("CommonApplicationData") + "\Microsoft\Windows\Start Menu\Programs"
-write-host $path
+$path = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs"
 $cmd = [Environment]::GetFolderPath("System") + "\cmd.exe"
 $dest = "$path\本部へリモートメンテ要請.lnk"
-write-host $dest
 if(-not(Test-Path $dest)){
     $shortcut = $shell.CreateShortcut($dest)
     $shortcut.TargetPath = $cmd
