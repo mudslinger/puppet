d094674c8dd09c821e0cc59346397e8f81f78779
mudslinger <hisato.tanaka@gmail.com>
2014-12-17 11:17:59 +0900

    '321'

--- modules/msra/templates/create_shortcut.ps1
+++ modules/msra/templates/create_shortcut.ps1
@@ -1,16 +1,11 @@
 $shell = New-Object -ComObject WScript.Shell
-
 $path = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs"
 $cmd = [Environment]::GetFolderPath("System") + "\cmd.exe"
 $dest = "$path\remoteassistance.lnk"
-try{
-    $shortcut = $shell.CreateShortcut($dest)
-    $shortcut.TargetPath = $cmd
-    $shortcut.Arguments = "/C C:\setup\remotehelp.bat"
-    $shortcut.WorkingDirectory = "c:\setup"
-    $shortcut.IconLocation = [Environment]::GetFolderPath("System") + "msra.exe,0"
-    $shortcut.WindowStyle = 7
-    $shortcut.save()
-}catch{
-    write-host 'could not create shortcut'
-}
+$shortcut = $shell.CreateShortcut($dest)
+$shortcut.TargetPath = $cmd
+$shortcut.Arguments = "/C C:\setup\remotehelp.bat"
+$shortcut.WorkingDirectory = "c:\setup"
+$shortcut.IconLocation = "msra.exe,0"
+$shortcut.WindowStyle = 7
+$shortcut.save()
