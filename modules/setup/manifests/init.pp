89f0005cd7f91fb3b27910b2bc77ab287f69a606
mudslinger <hisato.tanaka@gmail.com>
2014-12-17 18:28:58 +0900

    '321'

--- modules/setup/manifests/init.pp
+++ modules/setup/manifests/init.pp
@@ -2,4 +2,16 @@ class setup($setupdir = "c:\\setup"){
   file{ $setupdir:
     ensure => "directory"
   }
+  file {"$setupdir\\GoldappsFolder.itemdata-ms":
+    ensure => "file",
+    source => "puppet:///files/GoldappsFolder.itemdata-ms",
+    source_permissions => ignore,
+    require => File[$setupdir]
+  }
+  file {"$setupdir\\GoldappsFolder.itemdata-ms.bak":
+    ensure => "file",
+    source => "puppet:///files/GoldappsFolder.itemdata-ms.bak",
+    source_permissions => ignore,
+    require => File[$setupdir]
+  }
 }
