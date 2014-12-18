c6f76d78c89a025c70a637bd1b3fcfc56138e82f
mudslinger <hisato.tanaka@gmail.com>
2014-12-18 17:32:31 +0900

    '1231ricoh'

--- templates/ricoh/install_c305.ps1
+++ templates/ricoh/install_c305.ps1
@@ -3,13 +3,14 @@ pnpUtil -i -a C:\setup\176757\oemsetup.inf
 pnpUtil -i -a c:\setup\176758\oemsetup.inf
 
 #install driver by powershell
-Add-PrinterDriver -Name "RICOH RPCS Basic BW"
+
 Add-PrinterDriver -Name "RICOH RPCS Basic Color"
+Add-PrinterDriver -Name "RICOH RPCS Basic BW"
 
 #create printer port
-Add-PrinterPort -PrinterHostAddress 192.168.128.8 -Name "IP_192.168.128.8_BW"
 Add-PrinterPort -PrinterHostAddress 192.168.128.8 -Name "IP_192.168.128.8_Color"
+Add-PrinterPort -PrinterHostAddress 192.168.128.8 -Name "IP_192.168.128.8_BW"
 
 #create printer
-Add-Printer -Name "C305_MONO" -DriverName "RICOH RPCS BASIC BW" -PortName "IP_192.168.128.8_BW"
 Add-Printer -Name "C305_COLOR" -DriverName "RICOH RPCS BASIC Color" -PortName "IP_192.168.128.8_Color"
+Add-Printer -Name "C305_MONO" -DriverName "RICOH RPCS BASIC BW" -PortName "IP_192.168.128.8_BW"
