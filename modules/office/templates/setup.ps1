a47f36a3d08301a877f43cbe6e2831c397eded27
mudslinger <hisato.tanaka@gmail.com>
2014-12-22 17:54:18 +0900

    'officefull'

--- modules/office/templates/setup.ps1
+++ modules/office/templates/setup.ps1
@@ -1,6 +1,6 @@
-$platform = $null
-try{
-$platform = (Get-ItemProperty 'HKLM:\\SOFTWARE\\Microsoft\\Office\\15.0\\ClickToRun\\Configuration\\').platform
-}catch{
+# $platform = $null
+# try{
+# $platform = (Get-ItemProperty 'HKLM:\\SOFTWARE\\Microsoft\\Office\\15.0\\ClickToRun\\Configuration\\').platform
+# }catch{
 Invoke-Expression -Command "<%=@grvpath%>\setup.exe /configure <%=@confxmlpath%>"
-}
+# }
