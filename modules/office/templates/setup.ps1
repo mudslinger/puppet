4ccd786ceedf6e9d382ccd3e3f92ef86c1596e48
mudslinger <hisato.tanaka@gmail.com>
2014-12-22 19:10:30 +0900

    'office2'

--- modules/office/templates/setup.ps1
+++ modules/office/templates/setup.ps1
@@ -1,6 +1,9 @@
-# $platform = $null
-# try{
-# $platform = (Get-ItemProperty 'HKLM:\\SOFTWARE\\Microsoft\\Office\\15.0\\ClickToRun\\Configuration\\').platform
-# }catch{
-Invoke-Expression -Command "<%=@grvpath%>\setup.exe /configure <%=@confxmlpath%>"
-# }
+$platform = $null
+try{
+$platform = (Get-ItemProperty 'HKLM:\\SOFTWARE\\Microsoft\\Office\\15.0\\ClickToRun\\Configuration\\').platform
+if($platform -ne "X64"){
+<%=@grvpath%>\setup.exe /configure <%=@confxmlpath%>
+}
+}catch{
+<%=@grvpath%>\setup.exe /configure <%=@confxmlpath%>
+}
