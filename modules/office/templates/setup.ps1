105baf77315f51d60e11b1b51a95329ef82f9590
mudslinger <hisato.tanaka@gmail.com>
2014-12-22 16:23:48 +0900

    '1231'

--- modules/office/templates/setup.ps1
+++ modules/office/templates/setup.ps1
@@ -1,4 +1,4 @@
 $platform = (Get-ItemProperty 'HKLM:\\SOFTWARE\\Microsoft\\Office\\15.0\\ClickToRun\\Configuration\\').platform
-if($platform -ne 'X64')){
+if($platform -ne 'X64'){
 Invoke-Expression -Command "<%=@grvpath%>\setup.exe /configure <%=@confxmlpath%>"
 }
