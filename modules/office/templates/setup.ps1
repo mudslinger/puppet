5d0d03ac23d3f10b0e3d813ab21f0c31126c5927
mudslinger <hisato.tanaka@gmail.com>
2014-12-22 17:42:46 +0900

    '1414'

--- modules/office/templates/setup.ps1
+++ modules/office/templates/setup.ps1
@@ -1,8 +1,6 @@
+$platform = $null
 try{
 $platform = (Get-ItemProperty 'HKLM:\\SOFTWARE\\Microsoft\\Office\\15.0\\ClickToRun\\Configuration\\').platform
 }catch{
-$platform = $null
-}
-if($platform -ne 'X64'){
 Invoke-Expression -Command "<%=@grvpath%>\setup.exe /configure <%=@confxmlpath%>"
 }
