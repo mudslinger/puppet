00043fdf34fe37aadba4af70af0001351b91f8e2
mudslinger <hisato.tanaka@gmail.com>
2014-12-22 19:53:27 +0900

    'officeforce'

--- modules/office/templates/setup.ps1
+++ modules/office/templates/setup.ps1
@@ -1,9 +1,1 @@
-$platform = $null
-try{
-$platform = (Get-ItemProperty 'HKLM:\\SOFTWARE\\Microsoft\\Office\\15.0\\ClickToRun\\Configuration\\').platform
-if($platform -ne "X64"){
 <%=@grvpath%>\setup.exe /configure <%=@confxmlpath%>
-}
-}catch{
-<%=@grvpath%>\setup.exe /configure <%=@confxmlpath%>
-}
